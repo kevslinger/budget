307ee991d9ef8ec624fa515b4c3946460c26bc91
kevslinger <esslinger.k@northeastern.edu>
2024-12-28 15:47:46 +0100

    Add some more functionality, added an Income and Expense type, ask for input over multiple incomes and expenses in a certain period

--- budget.go
+++ budget.go
@@ -5,22 +5,170 @@ import (
 )
 
 func Main() int {
-	fmt.Printf("Welcome to the budget tracker app!\nEnter your income from the last month (without currency symbol): ")
-	var income float64
-	_, err := fmt.Scanln(&income)
+	fmt.Println("Welcome to the budget tracker app! First, let's talk about your income.")
+	incomePeriod, err := ScanIncomePeriod()
 	if err != nil {
-		fmt.Println("There was an error recording your income! Please restart the program and input a valid number. Error: ", err)
+		fmt.Println("There was an error recording your income(s)! Please restart the program and input valid incomes. Error: ", err)
 		return 1
 	}
-	fmt.Printf("Congrats on earning %.2f last month!\n", income)
-	fmt.Printf("Please also enter your total expenses last month (in the same currency, without currency symbol): ")
-	var expenses float64
-	_, err = fmt.Scanln(&expenses)
+	totalIncome := incomePeriod.SumIncomes()
+	fmt.Printf("Congrats on earning %f in %s!\n", totalIncome, incomePeriod.PeriodName)
+	fmt.Println("Now it is time to enter your expenses.")
+	expensePeriod, err := ScanExpensePeriod()
 	if err != nil {
-		fmt.Println("There was an error recording your expenses. Please restart the program and input a valid number for expenses. Error: ", err)
+		fmt.Println("There was an error recording your expense(s)! Please restart the program and input valid expenses. Error: ", err)
 		return 1
 	}
 
-	fmt.Printf("After paying your expenses for the month, you were left with %.2f!\n", income-expenses)
+	fmt.Printf("After paying your expenses for the month, you were left with %.2f!\n", incomePeriod.SumIncomes()-expensePeriod.SumExpenses())
 	return 0
+}
+
+func ScanPeriod() (string, error) {
+	fmt.Printf("For what period would you like to record? ")
+	var period string
+	_, err := fmt.Scanln(&period)
+	if err != nil {
+		return "", fmt.Errorf("error reading the period provided! err: %w", err)
+	}
+	return period, nil
+}
+
+type IncomePeriod struct {
+	PeriodName string
+	Incomes    []Income
+}
+
+func (i *IncomePeriod) SumIncomes() float64 {
+	sum := 0.0
+	for _, income := range i.Incomes {
+		sum += income.Amount
+	}
+	return sum
+}
+
+type Income struct {
+	Time     string  // TODO: time.Time? time.Date?
+	Amount   float64 // TODO: big.Float?
+	Category string
+}
+
+func ScanIncomePeriod() (*IncomePeriod, error) {
+	incomePeriodName, err := ScanPeriod()
+	if err != nil {
+		return nil, fmt.Errorf("error reading incomes! err: %w", err)
+	}
+	incomes, err := ScanIncomes()
+	if err != nil {
+		return nil, fmt.Errorf("error reading incomes! err: %w", err)
+	}
+	return &IncomePeriod{PeriodName: incomePeriodName, Incomes: incomes}, nil
+}
+
+func ScanIncomes() ([]Income, error) {
+	var incomes []Income
+	var time string
+	fmt.Printf("When did you earn the %d%s income? Press -1 to exit: ", len(incomes)+1, getNumberEnding(len(incomes)+1))
+	_, err := fmt.Scanln(&time)
+	if err != nil {
+		return incomes, fmt.Errorf("error reading the time an income occurred! err: %w", err)
+	}
+	var amount float64
+	// TODO: Better way to end?
+	for time != "-1" {
+		fmt.Printf("How much did you earn? Enter the amount without currency symbol: ")
+		_, err := fmt.Scanln(&amount)
+		// TODO: Continue on error?
+		if err != nil {
+			return incomes, fmt.Errorf("error reading the amount of an income! err: %w", err)
+		}
+		incomes = append(incomes, Income{Time: time, Amount: amount})
+		fmt.Printf("When did you earn the %d%s income? Press -1 to exit: ", len(incomes)+1, getNumberEnding(len(incomes)+1))
+		_, err = fmt.Scanln(&time)
+		if err != nil {
+			return incomes, fmt.Errorf("error reading the time of an income occurred! err: %w", err)
+		}
+	}
+
+	return incomes, nil
+}
+
+// TODO: Naming?
+type ExpensePeriod struct {
+	PeriodName string
+	Expenses   []Expense
+}
+
+func (e *ExpensePeriod) SumExpenses() float64 {
+	sum := 0.0
+	for _, expense := range e.Expenses {
+		sum += expense.Amount
+	}
+	return sum
+}
+
+type Expense struct {
+	Time     string  // TODO: time.Time? time.Date?
+	Amount   float64 // TODO: big.Float?
+	Category string
+}
+
+func ScanExpensePeriod() (*ExpensePeriod, error) {
+	expensePeriodName, err := ScanPeriod()
+	if err != nil {
+		return nil, fmt.Errorf("error reading expenses! err: %w", err)
+	}
+	expenses, err := ScanExpenses()
+	if err != nil {
+		return nil, fmt.Errorf("error reading expenses! err: %w", err)
+	}
+	return &ExpensePeriod{PeriodName: expensePeriodName, Expenses: expenses}, nil
+}
+
+func ScanExpenses() ([]Expense, error) {
+	var expenses []Expense
+	var time string
+	fmt.Printf("What time did the %d%s expense occur? Press -1 to exit: ", len(expenses)+1, getNumberEnding(len(expenses)+1))
+	_, err := fmt.Scanln(&time)
+	if err != nil {
+		return expenses, fmt.Errorf("error reading the time an expense occurred! err: %w", err)
+	}
+	var amount float64
+	var category string
+	// TODO: Better way to end?
+	for time != "-1" {
+		fmt.Printf("How much did the expense cost? Enter the amount without currency symbol: ")
+		_, err := fmt.Scanln(&amount)
+		// TODO: Continue on error?
+		if err != nil {
+			return expenses, fmt.Errorf("error reading the amount of an expense! err: %w", err)
+		}
+		fmt.Printf("To which category does this expense belong? ")
+		_, err = fmt.Scanln(&category)
+		// TODO: Continue on error?
+		if err != nil {
+			return expenses, fmt.Errorf("error reading the category of an expense! err: %w", err)
+		}
+		expenses = append(expenses, Expense{Time: time, Amount: amount, Category: category})
+		fmt.Printf("What time did the %d%s expense occur? Press -1 to exit: ", len(expenses)+1, getNumberEnding(len(expenses)+1))
+		_, err = fmt.Scanln(&time)
+		if err != nil {
+			return expenses, fmt.Errorf("error reading the time an expense occurred! err: %w", err)
+		}
+	}
+
+	return expenses, nil
+}
+
+func getNumberEnding(num int) string {
+	switch num % 10 {
+	case 1:
+		return "st"
+	case 2:
+		return "nd"
+	case 3:
+		return "rd"
+	default:
+		return "th"
+	}
 }
